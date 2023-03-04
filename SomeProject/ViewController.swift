0bd6f3f17ba909e4347dfd1f26baeb1739885abb
Olga Mikhailova <foxx_fire@mail.ru>
2023-03-04 21:40:17 +0300

    Initial Commit

---
+++ SomeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeProject
+//
+//  Created by mbpro2.0/16/512 on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
