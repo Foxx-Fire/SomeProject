5070784536113579b7732a03cefae91e54538b5b
Olga Mikhailova <foxx_fire@mail.ru>
2023-03-07 20:29:41 +0300

    add first commit
    -commit

--- SomeProject/ViewController.swift
+++ SomeProject/ViewController.swift
@@ -12,18 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        changeBGcolor()
-
- parent of 0b75bc9 (func color)
+    
         // Do any additional setup after loading the view.
     }
 
 
 }
 
-// first commit
-
-// second commit
 
 
-// feature
